09b42d2ae80d076720df233b954b15c316222071
HenryFabianBoadaRubio <156461496+HenryFabianBoadaRubio@users.noreply.github.com>
2024-09-04 03:48:48 -0500

    feat: :construction: Se genera configuracion de estilos pagina servicios
    Se realiza la configuracion de estilos de la pagina de servicios

--- public/js/carga.js
+++ public/js/carga.js
@@ -261,3 +261,28 @@ skills.forEach((skill, index) => {
 closeBtn.addEventListener('click', () => {
     infoCard.style.display = 'none';
 });
+
+
+// scripts.js
+let slideIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        slideIndex = 0;
+    } else if (index < 0) {
+        slideIndex = slides.length - 1;
+    } else {
+        slideIndex = index;
+    }
+    const offset = -slideIndex * 100;
+    document.querySelector('.carousel').style.transform = `translateX(${offset}%)`;
+}
+
+function moveSlide(step) {
+    showSlide(slideIndex + step);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(slideIndex);
+});
